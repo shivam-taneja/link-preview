dd220ad9dcc6d0445acea4ba20d77e34b5fb9664
Shivam Taneja <shiivamtaneja@gmail.com>
2025-12-16 15:25:08 +0530

    add: simple endpoint
    Signed-off-by: Shivam Taneja <shiivamtaneja@gmail.com>

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,56 @@
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { json, urlencoded } from 'express';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './common/filters/http-exception.filter';
+import { ValidationExceptionFilter } from './common/filters/validation-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 8000);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log', 'debug', 'verbose'],
+    cors: true,
+  });
+
+  const configService = app.get(ConfigService);
+
+  // Security
+  app.use(helmet());
+  app.enableCors({
+    origin: configService.get('CORS_ORIGIN', '*'),
+    credentials: true,
+  });
+
+  // Global prefix
+  app.setGlobalPrefix(configService.get('API_PREFIX', 'api'));
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
+  app.useGlobalFilters(
+    new AllExceptionsFilter(),
+    new ValidationExceptionFilter(),
+  );
+
+  app.use(json());
+  app.use(urlencoded({ extended: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  const port = configService.get('PORT', 8000);
+  await app.listen(port);
+
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
